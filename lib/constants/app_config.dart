d52619d8615b6878030b68765986bbfda92ea92e
Shyam <sastry.133@gmail.com>
2024-04-04 20:39:20 +0530

    App config update for production

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  static const String baseUrl = 'http://192.168.1.17:5000/api/mobileusers';
-  static const String normalUrl = 'http://192.168.1.17:5000/api';
+  static const String baseUrl = 'https://api.srbs1937.org/api/mobileusers';
+  static const String normalUrl = 'https://api.srbs1937.org/api';
   // static const String baseUrl = 'http://192.168.0.119:5000/api/mobileusers'; // vico
   // static const String normalUrl = 'http://192.168.0.119:5000/api'; // vico
   // static const String url = 'https://api.srbs1937.org';
